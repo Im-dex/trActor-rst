1e1e9361add6999735a86f1808f55c5ca25f9a66
a.tsukanov <tsukanov.anton@gmail.com>
2016-07-27 15:51:56 +0300

    A little bit more bounded mpsc queue implementation

--- src/concurrent/bounded_mpsc_queue.rs
+++ src/concurrent/bounded_mpsc_queue.rs
@@ -1,7 +1,48 @@
-struct Node<'T, T: 'T> {
-    pub value: &'T mut T
+extern crate core;
+
+use std::sync::atomic::AtomicPtr;
+use self::core::ptr;
+
+struct Node<T> {
+    pub value: Option<T>,
+    pub next: AtomicPtr<Node<T>>,
+    pub count: usize
+}
+
+impl<T> Node<T> {
+    fn new(value: T) -> Node<T> {
+        Node {
+            value: Some(value),
+            next: Node::null_ptr(),
+            count: 0
+        }
+    }
+
+    fn empty() -> Node<T> {
+        Node {
+            value: None,
+            next: Node::null_ptr(),
+            count: 0
+        }
+    }
+
+    fn null_ptr() -> AtomicPtr<Node<T>> {
+        AtomicPtr::new(ptr::null_mut::<Node<T>>())
+    }
 }
 
 pub struct BoundedMpscQueue<T> {
+    _head: AtomicPtr<Node<T>>,
+    _tail: AtomicPtr<Node<T>>
+}
 
+impl<T> BoundedMpscQueue<T> {
+    fn new(capacity: usize) -> BoundedMpscQueue<T> {
+        let empty = Node::empty();
+
+        BoundedMpscQueue {
+            _head: Node::null_ptr(),
+            _tail: Node::null_ptr()
+        }
+    }
 }
